c82e617af0282efe4cf2cf61be8acf7a01031982
anutr3008-max <anutr3008@gmail.com>
2025-11-25 10:41:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ def decode_review(seq):
 # ---------------------------
 # Load LSTM SavedModel folder
 # ---------------------------
-#model_path = "lstm_imdb_savedmodel"
-model_path = tf.keras.models.load_model("lstm_imdb_new.keras")
+model_path = "lstm_imdb_savedmodel"
 
 
 if not os.path.exists(model_path):
@@ -106,5 +105,6 @@ if st.button("Predict Review Sentiment"):
             st.success(f"Predicted: {pred} (prob={prob:.4f})")
         except Exception as e:
             st.error(f"Prediction failed: {e}")
+
 
 
